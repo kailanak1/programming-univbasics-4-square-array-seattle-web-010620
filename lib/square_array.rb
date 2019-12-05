95f2e7f64fc9457241f6c1cc3893fd668edfe9bd
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-05 02:44:58 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,9 @@
 
 def square_array(array)
   counter = 0
+  new_array = []
   while counter < array[counter] do
-    array[counter] * array[counter]
+    array[counter] ** 2
     counter += 1
   end 
   return array
